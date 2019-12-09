50c529386843f0460be84f2a6641bcd73f864f71
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-12-09 14:29:56 -0500

    remove some more eliot metadata

--- minimal.nix
+++ minimal.nix
@@ -1,16 +1,10 @@
 { lib, buildPythonPackage, fetchPypi, zope_interface, pyrsistent, boltons
 , hypothesis, testtools, pytest }:
 buildPythonPackage rec {
-  pname = "eliot";
-  version = "1.7.0";
+  pname = "minimal";
+  version = "0.0.1";
 
   src = ./.;
 
   checkInputs = [ zope_interface ];
-
-  meta = with lib; {
-    homepage = https://github.com/itamarst/eliot/;
-    description = "Logging library that tells you why it happened";
-    license = licenses.asl20;
-  };
 }
